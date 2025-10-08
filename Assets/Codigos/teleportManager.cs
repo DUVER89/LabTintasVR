cce57c5958039240171cc8a63128f03dfb2466ae
DUVER89 <duveralexanderpradasaboya@gmail.com>
2025-10-08 16:36:18 -0500

    07-10-25
    actualizacion fallida

--- Assets/Codigos/teleportManager.cs
+++ Assets/Codigos/teleportManager.cs
@@ -3,9 +3,11 @@ using UnityEngine;
 public class teleportManager : MonoBehaviour
 {
     [SerializeField] GameObject player;
+    [SerializeField] Animator transition;
 
     public void TeleportPlayer(Transform teleportPointRoom)
     {
+        transition.SetTrigger("Usuario");
         player.transform.position = teleportPointRoom.position;
     }
 }
