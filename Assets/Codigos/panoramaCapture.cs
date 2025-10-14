b620e3937c3f82a605437906f756fd51ea5dad5e
DUVER89 <duveralexanderpradasaboya@gmail.com>
2025-10-14 13:54:13 -0500

    14-10-25
    actualizacion

---
+++ Assets/Codigos/panoramaCapture.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+public class panoramaCapture : MonoBehaviour
+{
+    public Camera targetCamera;
+    public RenderTexture cubeMapleft;
+    public RenderTexture equirecRTR;
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)) Capture();
+
+    }
+    public void Capture()
+    {
+        targetCamera.RenderToCubemap(cubeMapleft);
+        cubeMapleft.ConvertToEquirect(equirecRTR);
+        Save(equirecRTR);
+    }
+
+    public void Save(RenderTexture rt)
+    {
+        Texture2D tex = new Texture2D(rt.width, rt.height);
+        RenderTexture.active = rt;
+        tex.ReadPixels(new Rect(0, 0, rt.width, rt.height), 0, 0);
+        RenderTexture.active = null;
+    }
+}
